58fdbcdbb3e8c62347a86f8df16edb9db1ebcda1
zubairahmedanas <amianasahmed@gmail.com>
2022-05-22 15:07:24 +0600

    not_string problem solution

--- cb_1.py
+++ cb_1.py
@@ -1,11 +1,23 @@
-def front_back(str):
-    if len(str) <= 1:
+# def front_back(str):
+#     if len(str) <= 1:
+#         return str
+#
+#     first = str[0]
+#     last = str[-1]
+#     middle = str[1:-1]
+#     total = last + middle + first
+#
+#     # last + mid + first
+#     return total
+
+
+def not_string(str):
+    if 'is not' in str:
+        word = 'not' + ' ' + str
+        return word
+    elif 'not' in str:
+        # word = 'not' + ' ' + str
         return str
-
-    first = str[0]
-    last = str[-1]
-    middle = str[1:-1]
-    total = last + middle + first
-
-    # last + mid + first
-    return total
+    else:
+        word = 'not' + ' ' + str
+        return word
