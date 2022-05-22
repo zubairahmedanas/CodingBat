9f995a0ac1ac44a6e3788772f873bc9d5ce6acf6
zubairahmedanas <amianasahmed@gmail.com>
2022-05-22 15:03:21 +0600

    front_back problem solution

---
+++ cb_1.py
@@ -0,0 +1,11 @@
+def front_back(str):
+    if len(str) <= 1:
+        return str
+
+    first = str[0]
+    last = str[-1]
+    middle = str[1:-1]
+    total = last + middle + first
+
+    # last + mid + first
+    return total
