3317fe18712f1fd773aeefa79a4ef18b37513263
Zubair Anas Ahmed <amianasahmed@gmail.com>
2022-06-11 12:59:40 +0600

    date_fashion problem

--- cb_4.py
+++ cb_4.py
@@ -1,11 +1,15 @@
 def date_fashion(you, date):
   # if (2<you <8 or 2<=date <8):
   # 	return 0
-  if (you >2 and date>2):
-    return 2
-  elif (2<you <8 or 2<=date <8):
+   if(you <= 2 or date <=2):
   	return 0
-  else: 
-    return 1
+  elif (you >=8 or date>=8):
+    return 2
+  # elif(you <= 2 or date <=2):
+  # 	return 0
+  else:
+  	return 1
+  
+ 
 
-print(date_fashion(5, 2))
+print(date_fashion(2, 9))
