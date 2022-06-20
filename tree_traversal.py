caf297a2d6c2def21f0b566e28819f41b7cdf742
zubairahmedanas <amianasahmed@gmail.com>
2022-06-20 17:51:18 +0600

    preorder tree traversal completed

---
+++ tree_traversal.py
@@ -0,0 +1,38 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root, "")
+        else:
+            print("not possible")
+            return False
+
+    def preorder_print(self, start, traversal):
+        if start:
+            traversal += (str(start.value))
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
+
+
+tree = BinaryTree(1)
+
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.left.left.left = Node(6)
+tree.root.right.left = Node(7)
+tree.root.right.right = Node(8)
+tree.root.right.right.left = Node(9)
+
+print(tree.print_tree("preorder"))
