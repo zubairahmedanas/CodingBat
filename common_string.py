e5fd820e88d879f2c34a8d9e35f645f7d76a3170
zubairahmedanas <amianasahmed@gmail.com>
2022-07-04 22:14:43 +0600

    longest common prefix solution

--- common_string.py
+++ common_string.py
@@ -1,16 +1,21 @@
-def prefix(str):
-    # for i in range(0,len(str[0])):
-    #     # print("the values are", str[0][i])
-    #     for s in str:
-    #         print("lets see ",s[i])
-    # small_word = min(len(liststr)))
-    # print(small_word)
-    str.sort()
-    print(str)
+def longestCommonPrefix(strs):
+    # strs.sort()
+    # minimum = min(len(strs[0]), len(strs[len(strs) - 1]))
+    # print("the minimum ,", minimum)
+    strs.sort(key=len)
+    minimum = strs
+    print(minimum)
+
+    result = ""
+    if (len(strs) == 0):
+        return result
+    for i in range(0, len(minimum[0])):
+        # print("the values of i :", minimum[0][i])
+        for j in minimum:
+            if j[i] != minimum[0][i]:
+                return result
+        result = result + minimum[0][i]
+        print(result)
 
 
-    # print(y)
-    x = min(str, key = len)
-    print(len(x))
-
-prefix(["asdasdaflower","flow","flight"])
+print(longestCommonPrefix(["flower", "flow", "flight"]))
