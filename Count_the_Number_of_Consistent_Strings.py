ec83aa9ee501ba9eb892b26121a3cd0ea18d2857
zubair ahmed anas <amianasahmed@gmail.com>
2022-09-24 01:12:23 +0600

    Count the number of constrain strings problem solved

---
+++ Count_the_Number_of_Consistent_Strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        count = 0
+        for word in words:
+            if set(word).issubset(allowed):
+                count +=1
+        return count
